fa1c87043f996b7fbe246fbbeb7db6e2a693d5ea
Erick Lasluisa <erickl1709@gmail.com>
2024-09-02 10:58:22 -0500

    Docker

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -5,8 +5,7 @@ import { PrismaClient } from '@prisma/client';
 import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
-export class ClientsService extends PrismaClient implements OnModuleInit{
-
+export class ClientsService extends PrismaClient implements OnModuleInit {
   private readonly logger = new Logger('ProductsService');
 
   onModuleInit() {
@@ -16,19 +15,19 @@ export class ClientsService extends PrismaClient implements OnModuleInit{
 
   create(createClientDto: CreateClientDto) {
     return this.client.create({
-      data: createClientDto
-    })
+      data: createClientDto,
+    });
   }
 
   findAll() {
     return this.client.findMany({
-      where: { available: true }
+      where: { available: true },
     });
   }
 
   async findOne(id: number) {
     const client = await this.client.findUnique({
-      where: { clientId: id, available: true }
+      where: { clientId: id, available: true },
     });
 
     if (!client) {
@@ -39,18 +38,17 @@ export class ClientsService extends PrismaClient implements OnModuleInit{
   }
 
   async update(id: number, updateClientDto: UpdateClientDto) {
-    const {clientId: _, ...data} = updateClientDto;
+    const { clientId: _, ...data } = updateClientDto;
 
     let updatedProduct;
     try {
       updatedProduct = await this.client.update({
         where: { clientId: id, available: true },
-        data: data
-      })
+        data: data,
+      });
     } catch (e) {
       throw new RpcException(`Product #${id} not found`);
     }
-
 
     return updatedProduct;
   }
@@ -61,8 +59,8 @@ export class ClientsService extends PrismaClient implements OnModuleInit{
     try {
       client = await this.client.update({
         where: { clientId: id, available: true },
-        data: { available: false }
-      })
+        data: { available: false },
+      });
     } catch (e) {
       throw new RpcException(`Product #${id} not found`);
     }
@@ -72,9 +70,9 @@ export class ClientsService extends PrismaClient implements OnModuleInit{
   async validateClients(clientId: number) {
     const client = await this.client.findUnique({
       where: {
-        clientId: clientId, 
-        available: true
-      }
+        clientId: clientId,
+        available: true,
+      },
     });
 
     if (!client) {
@@ -83,7 +81,4 @@ export class ClientsService extends PrismaClient implements OnModuleInit{
 
     return client;
   }
-
-
-
 }
--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,18 +1,18 @@
-import { IsString } from "class-validator";
+import { IsString } from 'class-validator';
 
 export class CreateClientDto {
-    @IsString()
-    name: string;
+  @IsString()
+  name: string;
 
-    @IsString()
-    ci: string;
+  @IsString()
+  ci: string;
 
-    @IsString()
-    email: string;
+  @IsString()
+  email: string;
 
-    @IsString()
-    phone: string;
+  @IsString()
+  phone: string;
 
-    @IsString()
-    address: string;
+  @IsString()
+  address: string;
 }
